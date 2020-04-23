26ae0c605432e2f97d77257e95431935e729db00
romain <romain78730@hotmail.fr>
2020-04-23 18:46:37 +0200

    storybook | setup

---
+++ .storybook/preview.js
@@ -0,0 +1,2 @@
+// import '!style-loader!css-loader!sass-loader!./main.scss';
+import '../src/styles/app.scss'
---
+++ src/stories/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default { title: 'Section Intro' };
+
+export const introText = () => (
+    <div className="intro-title">
+        Titre
+    </div>
+);
