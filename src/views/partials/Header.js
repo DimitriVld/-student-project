79190d73eb36efca45a11c40ea0cdd1b970fe449
DimitriVld <Dimitri.vildina@gmail.com>
2020-04-24 16:08:50 +0200

    feature/Layout add layout

--- src/views/partials/Header.js
+++ src/views/partials/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header = () => {
     return(
-        <Header className={'header-partial'} >
+        <header className={'header-partial'} >
             <h1> This is a header partial </h1>
-        </Header>
+        </header>
     )
 }
 
