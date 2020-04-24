38e96d5b53fd55a460f4735fa253f0bb44496e68
romain <romain78730@hotmail.fr>
2020-04-24 18:37:08 +0200

    noise + compétences

---
+++ src/constants/competences.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+    competences : [
+        {
+            "id" : 0,
+            "name" : "Design",
+            "content" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        },
+
+        {
+            "id" : 1,
+            "name" : "Développement Front",
+            "content" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." 
+        },
+
+        {
+            "id" : 2,
+            "name" : "Développement Back",
+            "content" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." 
+        },
+
+        {
+            "id" : 3,
+            "name" : "Conseils",
+            "content" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." 
+        }
+    ]
+}
