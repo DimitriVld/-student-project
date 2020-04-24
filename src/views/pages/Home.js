9598c1cf1027af556fa7fe6f3ca41f829f5f4e86
DimitriVld <Dimitri.vildina@gmail.com>
2020-04-24 19:49:27 +0200

    feature/Intro-section finish intro section

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import Intro from '../atoms/ui/Intro';
+import TitlePage from '../atoms/ui/TitlePage';
+import Icon from '../atoms/icons/Index';
 
 const Home = () => {
     return (
         <div className={'home-page'}>
             <div className="container-dft">
                 <section className="banner">
-                    <Intro 
+                    <TitlePage 
                         titleFirst={'We create <br/> & build'} 
                         titleSecond={'Digital solution'} 
                         contentText={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
                     />
+
+                    <Icon name="Smile" width={300}/>
                 </section>
             </div>
 
