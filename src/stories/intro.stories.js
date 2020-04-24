be7d7d728e128b7a263cf313359b0dc296490cc6
Romain Metayer <33374185+metayerromain@users.noreply.github.com>
2020-04-24 11:51:37 +0200

    Update intro.stories.js

--- src/stories/intro.stories.js
+++ src/stories/intro.stories.js
@@ -6,5 +6,5 @@ export const introText = () => (
     <div className="intro-title">
         We create & build
         <span className="intro-title-outline">Digital solution</span>
-    </div>s
+    </div>
 );
