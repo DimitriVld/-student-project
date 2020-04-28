0a3163adfb4779e9fb0228516886b31a85a0ed6f
romain <romain78730@hotmail.fr>
2020-04-28 23:09:07 +0200

    start about page

--- src/views/pages/AboutUs.js
+++ src/views/pages/AboutUs.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import TitlePage from '../atoms/TitlePage';
 
 const AboutUsPage = () => {
     return(
-        <div className={'about-us-page'}>
-            <p>about us page</p>
+        <div className={'about-us-page section'}>
+            <div className="container-dft">
+
+                <section className="banner">
+                    {/*
+                        # Title Page component 
+                        # Contain Title
+                        # contentText (facultative)
+                    */}
+                    <TitlePage 
+                        titleFirst={'A propos'} 
+                        titleSecond={'de nous'} 
+                        contentText={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
+                    />
+                </section>
+                
+                <section className="team">
+
+                    <div className="teammate">
+
+                        <div className="teammate-inner">
+                            <div className="teammate-image"></div>
+
+                        </div>
+
+                    </div>
+
+                </section>
+
+            </div>
         </div>
     )
 }
